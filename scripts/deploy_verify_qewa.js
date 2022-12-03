a06967bf1932d730bc0cdd8ac65db0dd0c2aab94
prolingua <prolingua.geo@gmail.com>
2022-12-03 12:24:06 +0000

    Create deploy_verify_qewa.js

---
+++ scripts/deploy_verify_qewa.js
@@ -0,0 +1,41 @@
+const { ethers } = require("hardhat");
+
+const etherToWei = (n) => {
+    return ethers.utils.parseEther(n.toString())
+  };
+
+const FEE = etherToWei(0.005);
+
+async function main() {
+    const FullFeatureToken = await ethers.getContractFactory("FullFeatureToken");
+    const fullFeatureToken = await FullFeatureToken.deploy(
+        "QEWA", 
+        "QEWA1", 
+        {"type":"BigNumber","hex":"0x513206"},
+        18,
+        "0x001345463dAa9ec237242d04c74c4f4c87AEd2E2",
+        {"_isBlacklistEnabled":false,"_isBurnable":false,"_isDocumentAllowed":false,"_isMintable":false,"_isPausable":false,"_isWhitelistEnabled":false,"_isMaxAmountOfTokensSet":false,"_isForceTransferAllowed":false},
+        {"type":"BigNumber","hex":"0x00"},
+        "",
+        "0x9254C0fcB2FaA4550B9ba582558cE1D03Ba3d05d",
+        { value: FEE.toString() }
+        );
+    await fullFeatureToken.deployed();
+    console.log("FullFeatureToken deployed to: ", fullFeatureToken.address);
+
+    await hre.run("verify:verify", {
+        address: fullFeatureToken.address,
+        constructorArguments: [
+          "QEWA", 
+          "QEWA1", 
+          {"type":"BigNumber","hex":"0x513206"},
+          18,
+          "0x001345463dAa9ec237242d04c74c4f4c87AEd2E2",
+          {"_isBlacklistEnabled":false,"_isBurnable":false,"_isDocumentAllowed":false,"_isMintable":false,"_isPausable":false,"_isWhitelistEnabled":false,"_isMaxAmountOfTokensSet":false,"_isForceTransferAllowed":false},
+          {"type":"BigNumber","hex":"0x00"},
+          "",
+          "0x9254C0fcB2FaA4550B9ba582558cE1D03Ba3d05d",
+        ],
+      })
+}
+main();
